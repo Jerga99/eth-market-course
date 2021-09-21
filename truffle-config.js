9de1d7126c9ca6a8f68b37be47e684417f3a2250
Filip Jerga <jerga99@gmail.com>
2021-09-21 15:05:41 +0200

    recap + mainet

--- truffle-config.js
+++ truffle-config.js
@@ -24,6 +24,22 @@ module.exports = {
       gasPrice: 20000000000, // how much we are willing to spent for unit of gas
       confirmations: 2, // number of blocks to wait between deployment
       timeoutBlocks: 200 // number of blocks before deployment times out
+    },
+    live: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://mainet.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: 1,
+      gas: 2500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 71000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200, // number of blocks before deployment times out
+      skipDryRun: true
     }
   },
   compilers: {
